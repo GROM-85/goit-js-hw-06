12fd2ed54f83a40e73c6c70d141fe2a25a81a57e
Roman Gordiienko <romangordiienko@Romans-MacBook-Pro.local>
2023-01-09 13:27:42 +0100

    6 remedy

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,10 @@
 const input = document.querySelector("#validation-input");
 
 input.addEventListener("blur",() => {
-    input.value.length == input.dataset.length ?
-    input.classList.add('valid') :
+    if(input.value.length == input.dataset.length){
+        input.classList.add('valid');
+        return;
+    } 
     input.classList.add('invalid');
 })
 
