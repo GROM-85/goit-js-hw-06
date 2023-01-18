c270a18d4bf7810a0be711888afce6a2d1b4396d
GROM-85 <richfron69@gmail.com>
2023-01-18 11:12:59 +0100

    form handling update

--- js/task-08.js
+++ js/task-08.js
@@ -3,14 +3,24 @@ const form = document.querySelector(".login-form");
 
 function formHandler(event){
     event.preventDefault();
-    const formData = new FormData(event.currentTarget);
+    let formData = {};
     let {email,password} = event.currentTarget.elements;
 
-    if(email.value !== "" && password.value !== ""){
-        formData.forEach((value,key) => {
-            console.log(`${key} :${value}`);
-        })
-        event.currentTarget.reset()
+    if(email && password){
+        // variant 1 to collect form data from new FormData()
+        for(let [key,value] of new FormData(event.currentTarget).entries()){
+            formData[key] = value;
+        }
+        //variant 2 via pseudo array event.currentTarget.elements and reduce
+        formData = [...event.currentTarget.elements].reduce((obj,curr) => {
+            if(curr.name){
+                obj[curr.name] = curr.value;
+            }  
+            return obj;
+        },{});
+
+        event.currentTarget.reset();
+        console.log(formData);
     }
     else{
         alert('All fields must be filled in!')
